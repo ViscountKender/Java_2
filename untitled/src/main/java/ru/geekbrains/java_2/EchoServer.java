2e27eb41c1c0966f0e99e2e59a5d5a0d75a55947
Viscount <siyanov.d@sar.garm.ru>
2022-06-15 00:45:53 +0400

    server and client

--- untitled/src/main/java/ru/geekbrains/java_2/EchoServer.java
+++ untitled/src/main/java/ru/geekbrains/java_2/EchoServer.java
@@ -1,4 +1,33 @@
 package ru.geekbrains.java_2;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            final Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            final DataInputStream in = new DataInputStream(socket.getInputStream());
+            final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            while (true) {
+                final String messag = in.readUTF();
+                if ("/end".equalsIgnoreCase(messag)) {
+                    out.writeUTF("/end");
+                    break;
+                }
+                System.out.println("Сообщение от клиента: " + messag);
+                out.writeUTF(messag);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
